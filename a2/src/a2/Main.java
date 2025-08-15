d0f9429e5dbc62d8bae780d00a4f035bef48ec33
Alisson <llielzinh@gmail.com>
2025-08-15 14:46:51 -0300

    first commit

---
+++ a2/src/a2/Main.java
@@ -0,0 +1,10 @@
+package a2;
+
+public class Main{
+	public static void main(String[] args) {
+		int num = 5;
+		int num2 = 12;
+		int resultado = num + num2;
+		System.out.println(resultado);
+	}
+}
