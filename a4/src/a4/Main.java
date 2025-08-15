75bf227f06bee764479a816f2ba0f76eb4630792
Alisson <llielzinh@gmail.com>
2025-08-15 17:20:24 -0300

    method

---
+++ a4/src/a4/Main.java
@@ -0,0 +1,12 @@
+package a4;
+
+public class Main{
+	public static void main(String[] args) {
+		System.out.println(Calculo(5,6));
+	}
+	
+	public static int Calculo(int a, int b) {
+		int resultado = a + b;
+		return resultado;
+	}
+}
